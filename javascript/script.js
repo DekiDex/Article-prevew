334733536743d608a6c674eb245c16dc3e51c629
DekiDex <89651615+DekiDex@users.noreply.github.com>
2021-11-28 19:56:32 +0100

    article

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn').onclick = function() {
+	document.querySelector('.btn-icons').classList.toggle('hidden');
+    document.querySelector('.btn-icons').classList.toggle('btntwo');
+    document.querySelector('.btn-icons').classList.remove('btn');
+
+}
+
